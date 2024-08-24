52e0463b2928889b506a1aa78ed53fcbf814a9b2
oort3x <clareshevket@gmail.com>
2024-08-24 22:22:23 +0100

    new lab

---
+++ 3.1 debugCode/debug_code.js
@@ -0,0 +1,39 @@
+function performOperation() {
+  // Get user input from input fields
+  let num1 = parseInt(document.getElementById("input1").value);
+  let num2 = parseInt(document.getElementById("input2").value);
+  // Check if inputs are valid numbers
+  if (!isNaN(num1) && !isNaN(num2)) {
+    // Perform the operation
+    let result = multiply(num1, num2);
+
+    // Display the result
+    displayResult(result);
+  } else {
+    displayResult("Please enter valid numbers");
+  }
+}
+
+function multiply(a, b) {
+  // Introduce a debugger statement to pause execution
+  debugger;
+
+  // Multiply the numbers
+  return a * b;
+}
+
+function displayResult(result) {
+  // Display the result in the paragraph element
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = `The result is: ${result}`;
+}
+
+// performOperation() Function:
+// Retrieves numerical values entered by the user from HTML input fields (input1 and input2).
+// Validates that the entered values are valid numbers.
+// If both values are valid numbers, it calls the multiply() function passing these values, otherwise, it displays an error message.
+// multiply() Function:
+// Includes a debugger statement to pause code execution at this point for debugging purposes.
+// Multiplies two input numbers (a and b) and returns the result.
+// displayResult() Function:
+// Displays the result of the multiplication or an error message in a designated paragraph element (resultElement) on the webpage.
